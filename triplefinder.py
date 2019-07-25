0c15dd15b39b1834604273631c04278305ae2f05
Cooper Bosch <coopercb333@gmail.com>
2019-07-25 15:32:24 -0400

    fixed pickle creation

--- triplefinder.py
+++ triplefinder.py
@@ -3,15 +3,56 @@ import numpy as np
 import pickle
 from urllib.request import urlopen
 import matplotlib.pyplot as plt
+import os.path
+from os import path
 
-def get_triple():
+#path_to_captions = "/Users/cooperbosch/Desktop/CogWorks/semantic-image-search/captions_train2014.json"
+path_to_captions = "captions_train2014.json"
 
-    with open('resnet18_features.pkl', 'rb') as f:
-        data = pickle.load(f)
+with open(path_to_captions, "rb") as f:
+    captions_train = json.load(f)
+imageid_to_captions=None
+imageid_to_url=None
+if not path.exists("imageid_to_captions.pickle"):
+    imageid_to_captions = dict()
+    for image in captions_train['images']:
+        imageid_to_captions.update({image['id']: []})
+    for annotation in captions_train['annotations']:
+        imageid_to_captions[annotation['image_id']].append(annotation['caption'])
 
+    pickle_out = open("imageid_to_captions.pickle", "wb")
+    pickle.dump(imageid_to_captions, pickle_out)
+    pickle_out.close()
+else:
     pickle_in = open("imageid_to_captions.pickle", "rb")
     imageid_to_captions = pickle.load(pickle_in)
     pickle_in.close()
+
+if not path.exists('imageid_to_url.pickle'):
+    imageid_to_url = dict()
+    for image in captions_train['images']:
+        imageid_to_url[image['id']] = image['coco_url']
+
+    pickle_out = open("imageid_to_url.pickle", "wb")
+    pickle.dump(imageid_to_url, pickle_out)
+    pickle_out.close()
+else:
+    pickle_in = open("imageid_to_url.pickle", "rb")
+    imageid_to_url = pickle.load(pickle_in)
+    pickle_in.close()
+
+
+with open('resnet18_features.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+
+list_of_imageids = [k for k in data]
+
+
+
+def get_triple():
+
+
 
     random_index = int(np.random.randint(0, len(list_of_imageids)))
     image_id = list_of_imageids[random_index]
@@ -22,3 +63,12 @@ def get_triple():
     image_id = list_of_imageids[random_index]
     bad_image = data[image_id]
     return good_image, caption, bad_image
+
+def get_all_captions():
+
+    all_captions = []
+    for image_id in list_of_imageids:
+        all_captions.append(' '.join(caption for caption in imageid_to_captions[image_id]))
+
+    return all_captions
+
